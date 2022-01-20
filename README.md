f0c2f2d2f8ed382a58ef8a55872b4f9c62d8027a
uae <aniekaudo@yahoo.com>
2022-01-20 07:25:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Awesome-books
+
+This project is a basic website that allows users to add/remove books from a list.
+
+This project is part of the Microverse curriculum Module 2 Block 1. The main objective is of this is to understand how to use JavaScript objects and arrays dynamically and to modify the DOM and add basic events. This will follow professional guidlines in responsive design.
+
+# Live Demo
+
+[Live Demo](https://Anny85-code.github.io/Awesome-books/)
+
+# Built With
+
+    .HTML
+    .CSS
+    .HTML & CSS Linters
+
+# Getting Started
+
+Please get your browser update. To get a local copy up and running follow these simple steps.
+
+Run the command below from the Terminal:
+
+navigate to your projects folder
+
+run git clone (https://github.com/Anny85-code/AwesomeBook.git) in your terminals
+
+run cd microverse_portfolio
+
+start index.html
+
+# Authors
+
+👤 Author
+
+- GitHub: [github](https://github.com/VSeray)
+
+- Twitter: [twitter](https://twitter.com/home)
+
+- LinkedIn: [Linkdin](https://www.linkedin.com/in/vana-seraydarian-936687191/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BNyso4dw6Tz6UBL%2Fqkjvtvw%3D%3D)
+
+👤 Author
+
+- GitHub: [@Anny85-code](https://github.com/Anny85-code)
+
+- Twitter: @Annyudo8
+
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/aniekan-udo-665b65213/)
+
+🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/microverseinc/readme-template/issues).
+
+# Show your support
+
+Give a ⭐️ if you like this project! Acknowledgments
+
+Entire Microverse team
+My learning and coding partners
+
+📝 License
+
+This project is [MIT](https://github.com/microverseinc/readme-template/blob/master/MIT.md) licensed.
---
+++ bclasses.js
@@ -0,0 +1,114 @@
+const saveToLocalStorage = (key, data) =>
+  localStorage.setItem(key, JSON.stringify(data));
+const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+class Book {
+  constructor(title, author, id = undefined) {
+    this.title = title;
+    this.author = author;
+    if (id !== undefined) {
+      this.id = id;
+    }
+  }
+
+  getCode() {
+    return ` <tr>
+              <td>"${this.title}" by ${this.author}</td>
+              <td><button class='remove' data-id='${this.id}'>Remove</td>
+              </tr>`;
+  }
+
+  static add(book) {
+    const bookItems = this.getAllBooks();
+    let id = 1;
+    if (bookItems.length > 0) {
+      id = bookItems[bookItems.length - 1].id + 1;
+    }
+    book.id = id;
+    bookItems.push(book);
+    saveToLocalStorage('bookItems', bookItems);
+  }
+
+  static remove(id) {
+    let bookItems = this.getAllBooks();
+    bookItems = bookItems.filter((b) => b.id !== Number(id));
+    saveToLocalStorage('bookItems', bookItems);
+  }
+
+  static getAllBooks() {
+    let bookItems = getFromLocalStorage('bookItems');
+    if (bookItems === null) {
+      bookItems = [];
+    }
+    return bookItems;
+  }
+}
+
+function displayBooks() {
+  const bookItems = Book.getAllBooks();
+
+  const booksCode = bookItems.map((book) =>
+    new Book(book.title, book.author, book.id).getCode()
+  );
+  document.getElementById('bitems').innerHTML = booksCode.join('');
+
+  const removeButtons = Array.from(document.querySelectorAll('.remove'));
+  removeButtons.forEach((removeButton) => {
+    removeButton.addEventListener('click', (event) => {
+      const id = event.target.getAttribute('data-id');
+      Book.remove(id);
+      displayBooks();
+    });
+  });
+}
+
+displayBooks();
+
+const titleInput = document.getElementById('btitle');
+const authorInput = document.getElementById('bauthor');
+document.getElementById('bookslist').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
+
+  if (!title || !author) {
+    return;
+  }
+
+  const book = new Book(title, author);
+  Book.add(book);
+  displayBooks();
+});
+
+const listElement = document.getElementById('list');
+const formElement = document.getElementById('newbook');
+const contactElement = document.getElementById('contact');
+const listBody = document.querySelector('.header');
+const formBody = document.querySelector('.form');
+const contactBody = document.querySelector('.contactsection');
+
+listElement.addEventListener('click', () => {
+  listBody.style.display = 'block';
+  formBody.style.display = 'none';
+  contactBody.style.display = 'none';
+});
+
+formElement.addEventListener('click', () => {
+  formBody.style.display = 'block';
+  listBody.style.display = 'none';
+  contactBody.style.display = 'none';
+});
+
+contactElement.addEventListener('click', () => {
+  contactBody.style.display = 'flex';
+  listBody.style.display = 'none';
+  formBody.style.display = 'none';
+});
+
+window.addEventListener('load', () => {
+  const { DateTime } = luxon; /* eslint-disable-line no-undef */
+  this.today = DateTime.now();
+  document.getElementById('times').textContent = this.today.toLocaleString(
+    DateTime.DATETIME_MED
+  );
+});
